d8e5ef6613aa345906a642b9d3c0ea0d2c2d4c5b
dodgepong <dodgepong@gmail.com>
2017-01-31 22:28:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+API for FFG Upcoming Products
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, json, request
+from lxml import html
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def upcoming():
+    collection = request.args.get('collection', None)
+    page = requests.get('https://www.fantasyflightgames.com/en/upcoming/')
+    tree = html.fromstring(page.content)
+    scripts = tree.xpath('//script/text()')
+
+    for script in scripts:
+        if 'upcoming_data = [{' in script:
+            lines = script.split(';')
+            upcoming = json.loads(lines[0].split(' = ')[1])
+            if collection:
+                collection_upcoming = []
+                for entry in upcoming:
+                    if entry['root_collection'] == collection:
+                        collection_upcoming.append(entry)
+                upcoming = collection_upcoming
+            return jsonify({
+                'upcoming': upcoming
+            })
+        continue
+    return "There was an error retreiving the data."
+
+if __name__ == "__main__":
+    app.run(debug=True)
